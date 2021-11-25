2962e9b128d1ae1ef18385e55e364b8e3af74181
Franek Wysocki <wysocki.f.00@gmail.com>
2021-11-25 21:18:19 +0100

    Update models

--- server/BitPass/Domain/Model/AppUsers/AppUserSession.cs
+++ server/BitPass/Domain/Model/AppUsers/AppUserSession.cs
@@ -1,16 +1,17 @@
-#nullable enable
 namespace Domain.Model.AppUsers
 {
     public class AppUserSession
     {
         public long Id { get; private set; }
         public string RefreshToken { get; private set; }
+
+
         public string? IpAddress { get; private init; }
 
         private AppUserSession(string refreshToken, string? ipAddress)
         {
-            IpAddress = ipAddress;
             RefreshToken = refreshToken;
+            IpAddress = ipAddress;
         }
 
         public static AppUserSession Create(string refreshToken, string? ipAddress = null)
--- server/BitPass/Domain/Model/CipherLogins/CipherLogin.cs
+++ server/BitPass/Domain/Model/CipherLogins/CipherLogin.cs
@@ -1,13 +1,24 @@
-using System.Collections.Generic;
-
 namespace Domain.Model.CipherLogins
 {
     public class CipherLogin : Entity, IAggregateRoot
     {
-        public long Id { get; set; }
-        public long OwnerId { get; set; }
-        public string Identifier { get; set; }
-        public string EncryptedPassword { get; set; }
-        public string Url { get; set; }
+        public long Id { get; private set; }
+        public long OwnerId { get; private init; }
+        public string Identifier { get; private init; }
+        public string EncryptedPassword { get; private init; }
+        public string Url { get; private init; }
+
+        private CipherLogin(long ownerId, string identifier, string encryptedPassword, string url)
+        {
+            OwnerId = ownerId;
+            Identifier = identifier;
+            EncryptedPassword = encryptedPassword;
+            Url = url;
+        }
+
+        public static CipherLogin Create(long ownerId, string identifier, string encryptedPassword, string url)
+        {
+            return new CipherLogin(ownerId, identifier, encryptedPassword, url);
+        }
     }
 }

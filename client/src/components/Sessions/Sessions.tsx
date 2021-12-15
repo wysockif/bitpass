38147b8b9a82163608a873b904da85ce0260e75d
Franek Wysocki <wysocki.f.00@gmail.com>
2021-12-15 16:33:41 +0100

    Update active sessions page

--- client/src/components/Sessions/Sessions.tsx
+++ client/src/components/Sessions/Sessions.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PageTitle from "../PageTitile/PageTitle";
-import {Table} from "reactstrap";
+import {Button, Table} from "reactstrap";
 
 const Sessions = () => {
     return (
@@ -10,54 +10,31 @@ const Sessions = () => {
                 <Table striped>
                     <thead>
                     <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
+                        <th>IP address</th>
+                        <th>OS</th>
+                        <th>Browser</th>
+                        <th>Close session</th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                        <td>96.12.23.42</td>
+                        <td>Windows 10</td>
+                        <td>Chrome 96</td>
+                        <th><Button className="px-3 py-1">Close</Button></th>
                     </tr>
                     <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <td>86.12.22.49</td>
+                        <td>Windows 11</td>
+                        <td>Firefox 106</td>
+                        <th><Button className="px-3 py-1">Close</Button></th>
+
                     </tr>
                     <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">6</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">7</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
+                        <td>86.12.22.42</td>
+                        <td>MacOS 12.1</td>
+                        <td>Safari 9.1</td>
+                        <th><Button className="px-3 py-1">Close</Button></th>
                     </tr>
                     </tbody>
                 </Table>
